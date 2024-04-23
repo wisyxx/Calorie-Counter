276b83dc88937afb0959c816933ccb8d4981e94f
Álvaro Hernández <alvarohcolino@gmail.com>
2024-04-23 19:41:23 +0200

    Add static CV preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import GeneralInfoForm from './components/GeneralInfoForm';
 import EducationForm from './components/EducationForm';
+import './App.css';
 
 export default function App() {
   return (
@@ -14,6 +15,27 @@ export default function App() {
 
         <aside className="cv-container">
           {/* TO-DO: Create hard coded basic CV structure */}
+          <section className="cv-container__section general-info">
+            <h2 className="genaral-info__name">Álvaro Hernández</h2>
+            <div className="general-info__contact">
+              <p className="genaral-info__contact--email">
+                <span>Email:</span> alvarohcolino@gmail.com
+              </p>
+              <p className="genaral-info__contact--phone">
+                <span>Phone:</span> +34 608178843
+              </p>
+            </div>
+          </section>
+
+          <section className="cv-container__section education-info">
+            <h2 className="education-info__title">Education</h2>
+            <div className="education-info__container"> {/* This container will be dynamic */}
+              <h3 className="education-info__course">Course</h3>
+              <h3 className="education-info__school">School</h3>
+              <p className="education-info__start">From: 20-01-2000</p>
+              <p className="education-info__end">To: 12-10-2004</p>
+            </div>
+          </section>
         </aside>
       </main>
     </>
