ab757ccc3b2ab98400f452c4ef5c511999bf2a2c
Álvaro Hernández <alvarohcolino@gmail.com>
2024-04-24 10:28:38 +0200

    Add static experience preview

--- src/App.tsx
+++ src/App.tsx
@@ -29,11 +29,75 @@ export default function App() {
 
           <section className="cv-container__section education-info">
             <h2 className="education-info__title">Education</h2>
-            <div className="education-info__container"> {/* This container will be dynamic */}
-              <h3 className="education-info__course">Course</h3>
-              <h3 className="education-info__school">School</h3>
-              <p className="education-info__start">From: 20-01-2000</p>
-              <p className="education-info__end">To: 12-10-2004</p>
+            <div className="cv-container__entries ">
+              <div className="education-info__container">
+                {/* This container will be dynamic */}
+                <h3 className="education-info__course">Course</h3>
+                <h3 className="education-info__school">School</h3>
+                <p className="education-info__start">
+                  <span>From:</span> 20-01-2000
+                </p>
+                <p className="education-info__end">
+                  <span>To:</span> 12-10-2004
+                </p>
+              </div>
+
+              <div className="education-info__container">
+                {/* This container will be dynamic */}
+                <h3 className="education-info__course">Course</h3>
+                <h3 className="education-info__school">School</h3>
+                <p className="education-info__start">
+                  <span>From:</span> 20-01-2000
+                </p>
+                <p className="education-info__end">
+                  <span>To:</span> 12-10-2004
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <section className="cv-container__section experience">
+            <h2 className="experience__title">Experience</h2>
+            <div className="cv-container__entries">
+              <div className="experience__container">
+                {/* This container will be dynamic */}
+                <h3 className="experience__company">A company</h3>
+                <div className="experience__data">
+                  <p className="experience__job">
+                    <span>Posotion:</span> Job example
+                  </p>
+                  <p className="experience__description">
+                    <span>Description:</span> A really long description about my
+                    responsabilities in the company.
+                  </p>
+                  <p className="experience__start">
+                    <span>From:</span> 20-01-2000
+                  </p>
+                  <p className="experience__end">
+                    <span>To:</span> 12-10-2004
+                  </p>
+                </div>
+              </div>
+
+              <div className="experience__container">
+                {/* This container will be dynamic */}
+                <h3 className="experience__company">A company</h3>
+                <div className="experience__data">
+                  <p className="experience__job">
+                    <span>Posotion:</span> Job example
+                  </p>
+                  <p className="experience__description">
+                    <span>Description:</span> A really long description about my
+                    responsabilities in the company.
+                  </p>
+                  <p className="experience__start">
+                    <span>From:</span> 20-01-2000
+                  </p>
+                  <p className="experience__end">
+                    <span>To:</span> 12-10-2004
+                  </p>
+                </div>
+              </div>
             </div>
           </section>
         </aside>
