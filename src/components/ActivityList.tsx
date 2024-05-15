034895ddf47880eab0e2a1e3571078636062224f
Álvaro Hernández <alvarohcolino@gmail.com>
2024-05-15 15:46:52 +0200

    Add human readable category displaying

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,11 +1,19 @@
+import { useMemo } from 'react';
 import { Activity } from '../types';
+import { categories } from '../data/categories';
+
 
 type ActivityListProps = {
   activities: Activity[];
 };
 
 export const ActivityList = ({ activities }: ActivityListProps) => {
-  console.log(activities);
+  const categoryName = useMemo(
+    () => (category: Activity['category']) =>
+      categories.map((cat) => (cat.id === category ? cat.name : '')),
+    [activities]
+  );
+
   return (
     <>
       <h2 className=" text-4xl font-bold text-slate-600 text-center">
@@ -15,10 +23,16 @@ export const ActivityList = ({ activities }: ActivityListProps) => {
       {activities.map((activity) => (
         <div
           key={activity.id}
-          className=" px-5 bg-white mt-5 flex justify-between"
+          className=" px-5 py-10 bg-white mt-5 flex justify-between"
         >
           <div className=" space-y-2 relative">
-            <p>{activity.category === 1 ? 'Food' : 'Exercise'}</p>
+            <p
+              className={` absolute -top-8 -left-8 px-10 py-2 text-white font-bold uppercase ${
+                activity.category === 1 ? ' bg-lime-500' : ' bg-orange-500'
+              }`}
+            >
+              {categoryName(activity.category)}
+            </p>
             <p className=" text-2xl font-bold pt-5">{activity.name}</p>
             <p className=" font-black text-4xl text-lime-500">
               {activity.calories} <span>Kcal</span>
