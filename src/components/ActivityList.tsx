ac84db7cb3a6bc0b2a9c0d46edadde5c284721b0
Álvaro Hernández <alvarohcolino@gmail.com>
2024-05-17 19:39:27 +0200

    Add delete feature

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { categories } from '../data/categories';
-import { PencilSquareIcon } from '@heroicons/react/16/solid';
+import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/16/solid';
 import type { Activity } from '../types';
 import type { Dispatch } from 'react';
 import type { ActivityActions } from '../reducers/activityReducer';
@@ -50,6 +50,17 @@ export const ActivityList = ({ activities, dispatch }: ActivityListProps) => {
               }
             >
               <PencilSquareIcon className=" h-8 w-8 text-gray-800" />
+            </button>
+
+            <button
+              onClick={() =>
+                dispatch({
+                  type: 'delete-activity',
+                  payload: { id: activity.id },
+                })
+              }
+            >
+              <XCircleIcon className=" h-8 w-8 text-red-500" />
             </button>
           </div>
         </div>
--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -2,7 +2,8 @@ import { Activity } from '../types';
 
 export type ActivityActions =
   | { type: 'save-activity'; payload: { newActivity: Activity } }
-  | { type: 'set-activeId'; payload: { id: Activity['id'] } };
+  | { type: 'set-activeId'; payload: { id: Activity['id'] } }
+  | { type: 'delete-activity'; payload: { id: Activity['id'] } };
 
 export type ActivityState = {
   activities: Activity[];
@@ -40,6 +41,15 @@ export const activityReducer = (
     return {
       ...state,
       activeId: action.payload.id,
+    };
+  }
+
+  if (action.type === 'delete-activity') {
+    return {
+      ...state,
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
     };
   }
 
