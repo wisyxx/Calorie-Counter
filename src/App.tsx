2a7068b6bfcee1bfdd1de17f81f8951b89e803da
Álvaro Hernández <alvarohcolino@gmail.com>
2024-05-18 12:15:57 +0200

    Add restart button and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useMemo, useReducer } from 'react';
 import { Form } from './components/Form';
 import { activityReducer, initialState } from './reducers/activityReducer';
 import { ActivityList } from './components/ActivityList';
@@ -11,13 +11,31 @@ export const App = () => {
     [state.activities]
   );
 
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length > 0, [state.activities]);
+
   return (
     <>
       <header className=" bg-lime-600 py-3">
-        <div className="max-w-4 mx-auto flex justify-between">
+        <div className="max-w-4xl mx-auto flex items-center justify-between">
           <h1 className=" text-center text-lg font-bold text-white uppercase">
             Calorie counter
           </h1>
+
+          <button
+            onClick={() =>
+              dispatch({
+                type: 'restart-app',
+              })
+            }
+            disabled={!canRestartApp()} // If true disabled == false and vice-versa
+            className=" bg-gray-800 hover:bg-gray-900 p-2 font-bold 
+            uppercase text-white cursor-pointer rounded-lg text-sm 
+            transition-all duration-[250ms] disabled:opacity-50 
+          disabled:hover:bg-slate-800"
+          >
+            Reset App
+          </button>
         </div>
       </header>
 
--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -3,7 +3,8 @@ import { Activity } from '../types';
 export type ActivityActions =
   | { type: 'save-activity'; payload: { newActivity: Activity } }
   | { type: 'set-activeId'; payload: { id: Activity['id'] } }
-  | { type: 'delete-activity'; payload: { id: Activity['id'] } };
+  | { type: 'delete-activity'; payload: { id: Activity['id'] } }
+  | { type: 'restart-app' };
 
 export type ActivityState = {
   activities: Activity[];
@@ -60,6 +61,13 @@ export const activityReducer = (
       activities: state.activities.filter(
         (activity) => activity.id !== action.payload.id
       ),
+    };
+  }
+
+  if (action.type === 'restart-app') {
+    return {
+      activities: [],
+      activeId: '',
     };
   }
 
