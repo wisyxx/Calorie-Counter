35d2c8f1b90ebfcc370878d43e2b2dff77b04f3e
Álvaro Hernández <alvarohcolino@gmail.com>
2024-09-10 21:08:55 +0200

    Start using contextAPI

---
+++ src/contexts/ActivityContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, Dispatch, ReactNode, useReducer } from 'react';
+import {
+  ActivityActions,
+  activityReducer,
+  ActivityState,
+  initialState,
+} from '../reducers/activityReducer';
+
+type ActivityProviderProps = {
+  children: ReactNode;
+};
+type ActivityContextProps = {
+  state: ActivityState;
+  dispatch: Dispatch<ActivityActions>;
+};
+
+export const ActivityContext = createContext<ActivityContextProps>(null!);
+
+export const ActivityProvider = ({ children }: ActivityProviderProps) => {
+  const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  return (
+    <ActivityContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ActivityContext.Provider>
+  );
+};
